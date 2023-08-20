c61fccd847e85f90bb2e23938d8468668a2040f7
gonzalo.larghero <gonzalo.larghero@fing.edu.uy>
2023-08-20 00:57:48 -0300

    -animaciones funcionando raro

--- player/Player.gd
+++ player/Player.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 var borderLimit
 const MAX_SPEED = 200;
-const ACCELERATION = 20;
-const FRICTION = 20;
+const ACCELERATION = 700;
+const FRICTION = 700;
+
+@onready var animationPlayer = $AnimationPlayer;
+@onready var animationTree = $AnimationTree;
 
 func _ready():
 	borderLimit = get_viewport_rect().size
@@ -14,14 +17,15 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up");
 	input_vector = input_vector.normalized();
 	if(input_vector != Vector2.ZERO):
-		velocity += input_vector * ACCELERATION * delta;
-		velocity = velocity.limit_length(MAX_SPEED * delta);
+		animationTree.set("parameters/Idle/blend_position",input_vector);
+		animationTree.set("parameters/Run/blend_position",input_vector);
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta);
-	_animation_change()
+	
 	position.x = clamp(position.x, 0, borderLimit.x)
 	position.y = clamp(position.y, 0, borderLimit.y)
-	move_and_collide(velocity);
+	move_and_slide();
 	
 func _animation_change():
 	if velocity.x != 0:
