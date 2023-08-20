93dbd1540b1d019dc7bde6d644bbe4298ab1fb2d
gonzalo.larghero <gonzalo.larghero@fing.edu.uy>
2023-08-20 20:03:01 -0300

    -fixes

--- player/Player.gd
+++ player/Player.gd
@@ -8,9 +8,7 @@ const FRICTION = 700;
 @onready var animationPlayer = $AnimationPlayer;
 @onready var animationTree = $AnimationTree;
 @onready var animationState = animationTree.get("parameters/playback")
-
-func _ready():
-	borderLimit = get_viewport_rect().size
+@onready var boderlimit = get_viewport_rect().size
 	
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
@@ -18,9 +16,10 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up");
 	input_vector = input_vector.normalized();
 	
-	if(input_vector != Vector2.ZERO):
-		animationTree.set("parameters/Idle/blend_position",input_vector);
-		animationTree.set("parameters/Run/blend_position",input_vector);
+	animationTree.set("parameters/Idle/blend_position",input_vector);
+	animationTree.set("parameters/Run/blend_position",input_vector);
+	
+	if(input_vector != Vector2.ZERO):		
 		animationState.travel("Run");
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
