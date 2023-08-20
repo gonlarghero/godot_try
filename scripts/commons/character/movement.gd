13fa8080b9770cf041544294407c0e5ef35b3251
chrigonzalez <christian.gonzalez@mercadolibre.com>
2023-08-20 01:51:22 -0300

    add colission bush

--- scripts/commons/character/movement.gd
+++ scripts/commons/character/movement.gd
@@ -1,17 +1,21 @@
 extends Node
 
 const MAX_SPEED = 200
+const ACCELERATION = 700
+const FRICTION = 700
 
-func move_main_character(character, delta, player_sprite):  # Recibir las referencias como argumentos
+func move_main_character(character, delta):  # Recibir las referencias como argumentos
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO:
-		character.velocity = input_vector
+		character.velocity = character.velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
-		character.velocity = Vector2.ZERO
+		character.velocity = character.velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	character.move_and_collide(character.velocity * delta * MAX_SPEED)
+	character.move_and_slide()
 	
 func animate_main_character(character, player_sprite):
 	if character.velocity.x != 0:
--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -8,9 +8,7 @@ func _ready():
 	map_limit.init_limit(self)
 	
 func _physics_process(delta):
-	ch_movement.move_main_character(self, delta, $player_sprite)
+	ch_movement.move_main_character(self, delta)
 	ch_movement.animate_main_character(self, $player_sprite)
 	map_limit.set_limit(self);
-
-
 
