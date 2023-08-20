fa1c5a2fad4a279aefad2d770a80d8d5162c71c5
gonzalo.larghero <gonzalo.larghero@fing.edu.uy>
2023-08-19 21:52:29 -0300

    -cambia el movimiento

--- player/Player.gd
+++ player/Player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 var borderLimit
-const MAX_SPEED = 100;
+const MAX_SPEED = 200;
+const ACCELERATION = 20;
+const FRICTION = 20;
 
 func _ready():
 	borderLimit = get_viewport_rect().size
@@ -12,13 +14,14 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up");
 	input_vector = input_vector.normalized();
 	if(input_vector != Vector2.ZERO):
-		velocity = input_vector;
+		velocity += input_vector * ACCELERATION * delta;
+		velocity = velocity.limit_length(MAX_SPEED * delta);
 	else:
-		velocity = Vector2.ZERO		
+		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta);
 	_animation_change()
 	position.x = clamp(position.x, 0, borderLimit.x)
 	position.y = clamp(position.y, 0, borderLimit.y)
-	move_and_collide(velocity * delta * MAX_SPEED);
+	move_and_collide(velocity);
 	
 func _animation_change():
 	if velocity.x != 0:
