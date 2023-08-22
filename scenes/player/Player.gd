bef16fe9a23990898a514ab2aa11f122a0625d74
chrigonzalez <christian.gonzalez@mercadolibre.com>
2023-08-22 00:35:02 -0300

    bug in movement

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	move_main_character(delta)
 	map_limit.set_limit(self);
 
-func move_main_character(delta):  # Recibir las referencias como argumentos
+func move_main_character(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
