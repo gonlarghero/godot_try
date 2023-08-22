f532356010220b8e26ff2f7872733e1c4df138bd
chrigonzalez <christian.gonzalez@mercadolibre.com>
2023-08-21 23:32:27 -0300

    animation tree turbina

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -6,12 +6,15 @@ const MAX_SPEED = 200
 const ACCELERATION = 1000
 const FRICTION = 1000
 
+@onready var animationPlayer = $AnimationPlayer;
+@onready var animationTree = $AnimationTree;
+@onready var animationState = animationTree.get("parameters/playback")
+
 func _ready():
 	map_limit.init_limit(self)
 	
 func _physics_process(delta):
 	move_main_character(delta)
-	animate_main_character()
 	map_limit.set_limit(self);
 
 func move_main_character(delta):  # Recibir las referencias como argumentos
@@ -20,28 +23,14 @@ func move_main_character(delta):  # Recibir las referencias como argumentos
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	animationTree.set("parameters/Idle/blend_position",input_vector);
+	animationTree.set("parameters/Run/blend_position",input_vector);
+	
 	if input_vector != Vector2.ZERO:
+		animationState.travel("Run");
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
+		animationState.travel("Idle");
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	move_and_slide()
-	
-func animate_main_character():
-	if velocity.x != 0:
-		if velocity.x < 0:
-			$player_sprite.animation = "left"
-			$player_sprite.play("left")
-		else:
-			$player_sprite.animation = "right"
-			$player_sprite.play("right")
-	elif velocity.y != 0:
-		if velocity.y < 0:
-			$player_sprite.animation = "up"
-			$player_sprite.play("up")
-		else:
-			$player_sprite.animation = "down"
-			$player_sprite.play("down")
-	else:
-		$player_sprite.animation = "idle"
-		$player_sprite.play("idle")
