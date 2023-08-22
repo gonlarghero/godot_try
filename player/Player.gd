a3718a3fd4ab2310998f5fe73d441a338ad3b67f
gonzalo.larghero <gonzalo.larghero@fing.edu.uy>
2023-08-22 20:55:56 -0300

    - animaciones de ataques + wasd + assets de efectos

--- player/Player.gd
+++ player/Player.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
 var borderLimit
+var state = MOVE;
 const MAX_SPEED = 200;
 const ACCELERATION = 700;
 const FRICTION = 700;
@@ -11,8 +18,18 @@ const FRICTION = 700;
 
 func _ready():
 	borderLimit = get_viewport_rect().size;
+	animationTree.active = true;
 	
-func _physics_process(delta):
+func _process(delta):
+	match state:
+		MOVE: 
+			move_state(delta);
+		ROLL: 
+			roll_state(delta);
+		ATTACK:
+			attack_state(delta);
+					
+func move_state(delta):
 	var input_vector = Vector2.ZERO;
 	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left");
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up");
@@ -21,13 +38,28 @@ func _physics_process(delta):
 	if(input_vector != Vector2.ZERO):		
 		animationTree.set("parameters/Idle/blend_position",input_vector);
 		animationTree.set("parameters/Run/blend_position",input_vector);
+		animationTree.set("parameters/Attack/blend_position",input_vector);
 		animationState.travel("Run");
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
 		animationState.travel("Idle");
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta);
 	
+	if(Input.is_action_just_pressed("Attack")):
+		state = ATTACK;
+	
 	position.x = clamp(position.x, 0, borderLimit.x)
 	position.y = clamp(position.y, 0, borderLimit.y)
 	move_and_slide();
+
+func roll_state(delta):
+	pass;
 	
+func attack_state(delta):
+	velocity = Vector2.ZERO;
+	animationState.travel("Attack");
+
+func attack_ended():
+	state = MOVE;
+func roll_ended():
+	state = MOVE;
