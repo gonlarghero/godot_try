abeb473a2e0b6f34f72c68f642cd9a5884fe2bef
chrigonzalez <christian.gonzalez@mercadolibre.com>
2023-08-24 21:52:45 -0300

    test attack and change bush sprite

--- player/Player.gd
+++ player/Player.gd
@@ -3,16 +3,15 @@ extends CharacterBody2D
 enum{
 	MOVE,
 	ROLL,
-	ATTACK
+	ATTACK,
+	GONDA
 }
 
 var borderLimit
 var state = MOVE;
-const ROLL_SPEED = 270;
 const MAX_SPEED = 200;
 const ACCELERATION = 700;
 const FRICTION = 700;
-var roll_vector = Vector2.UP
 
 @onready var animationPlayer = $AnimationPlayer;
 @onready var animationTree = $AnimationTree;
@@ -22,7 +21,7 @@ func _ready():
 	borderLimit = get_viewport_rect().size;
 	animationTree.active = true;
 	
-func _physics_process(delta):
+func _process(delta):
 	match state:
 		MOVE: 
 			move_state(delta);
@@ -40,9 +39,7 @@ func move_state(delta):
 	if(input_vector != Vector2.ZERO):		
 		animationTree.set("parameters/Idle/blend_position",input_vector);
 		animationTree.set("parameters/Run/blend_position",input_vector);
-		animationTree.set("parameters/Attack/blend_position",input_vector);		
-		animationTree.set("parameters/Roll/blend_position",input_vector);
-		roll_vector = input_vector;
+		animationTree.set("parameters/Attack/blend_position",input_vector);
 		animationState.travel("Run");
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
@@ -51,18 +48,13 @@ func move_state(delta):
 	
 	if(Input.is_action_just_pressed("Attack")):
 		state = ATTACK;
-		
-	if(Input.is_action_just_pressed("Roll")):
-		state = ROLL;
 	
 	position.x = clamp(position.x, 0, borderLimit.x)
 	position.y = clamp(position.y, 0, borderLimit.y)
 	move_and_slide();
 
 func roll_state(delta):
-	velocity = roll_vector * ROLL_SPEED;
-	animationState.travel("Roll");
-	move_and_slide();
+	pass;
 	
 func attack_state(delta):
 	velocity = Vector2.ZERO;
