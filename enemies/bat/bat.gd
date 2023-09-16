76393064b8f7734da27f59f78ecdf0e8d66a8a03
gonzalo.larghero <gonzalo.larghero@fing.edu.uy>
2023-09-16 12:39:25 -0300

    -enemy movement

--- enemies/bat/bat.gd
+++ enemies/bat/bat.gd
@@ -1,14 +1,44 @@
 extends CharacterBody2D
 
 var knockback = Vector2.ZERO
+var state = IDLE;
 
+@onready var sprite = $Animation
 @onready var stats = $stats;
+@onready var player_detection_zone = $PlayerDetectionZone
+
+@export var MAX_SPEED = 500;
+@export var ACCELERATION = 200;
+@export var FRICTION = 200;
+
+enum{
+	IDLE,
+	WANDER,
+	CHASE
+}
 
 const enemy_death_effect_scn = preload("res://town/effects/enemy_death_effect.tscn");
 
 func _physics_process(delta):
-	velocity = velocity.move_toward(Vector2.ZERO, 200 * delta);
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				sprite.flip_h = velocity.x < 0
+			else:
+				state = IDLE	
 	move_and_slide()
+				
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
 	
 func _on_hurt_box_area_entered(area):
 	stats.health -= area.damage;
